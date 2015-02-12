917f4fb89220938e77750f227c84442f337144fd
maximri <maximri@wix.com>
2015-02-12 16:50:29 +0200

    Receiving a Response from the Auction

--- src/main/scala/com/wix/training/Main.scala
+++ src/main/scala/com/wix/training/Main.scala
@@ -2,10 +2,15 @@ package com.wix.training
 
 import javax.swing.SwingUtilities
 
+import com.wix.training.Main._
+import org.jivesoftware.smack.packet.Message
+import org.jivesoftware.smack.{Chat, MessageListener, XMPPConnection}
+
 class Main(args: String*) {
   var ui: MainWindow = _
 
   startUserInterface
+  val connection: XMPPConnection = XMPPConnection.connectTO(args(ARG_HOSTNAME), args(ARG_USERNAME), args(ARG_PASSWORD))
 
   private def startUserInterface() = {
     SwingUtilities.invokeAndWait(new Runnable {
@@ -14,14 +19,48 @@ class Main(args: String*) {
       }
     })
   }
+
+
+  def joinAuction(connection: XMPPConnection, itemID: String): Unit = {
+    connection.getChatManager.createChat(auctionId(itemID,connection),
+    new MessageListener {
+      override def processMessage(chat: Chat, message: Message): Unit = {
+        SwingUtilities.invokeLater(new Runnable {
+          override def run(): Unit = ui.showStatus(STATUS_LOST)
+        })
+      }
+    })
+  }
 }
 
 object Main {
 
   val STATUS_JOINING = "Joining"
-
   val SNIPER_STATUS_NAME = "sniper status"
-
   val STATUS_LOST = "lost"
 
+  val ARG_HOSTNAME = 0
+  val ARG_USERNAME = 1
+  val ARG_PASSWORD = 2
+  val ARG_ITEM_ID = 3
+
+  def auctionId(itemId: String, connection: XMPPConnection): String = {
+    String.format(XMPPConnection.AUCTION_ID_FORMAT, itemId, connection.getServiceName)
+  }
 }
+
+
+private object XMPPConnection {
+
+  val AUCTION_RESOURCE = "Auction"
+  val ITEM_ID_AS_LOGIN = "auction-%s"
+  val AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE
+
+  def connectTO(hostName: String, username: String, password: String): XMPPConnection = {
+    val connection: XMPPConnection = new XMPPConnection(hostName)
+    connection.connect()
+    connection.login(username, password, AUCTION_RESOURCE)
+    connection
+  }
+}
+
--- src/main/scala/com/wix/training/MainWindow.scala
+++ src/main/scala/com/wix/training/MainWindow.scala
@@ -2,37 +2,36 @@ package com.wix.training
 
 import java.awt.Color
 import javax.swing.border.LineBorder
-import javax.swing.{JLabel, JFrame}
+import javax.swing.{JFrame, JLabel}
 
-import com.wix.training.MainWindow.{MAIN_WINDOW_NAME, APP_TITLE}
+import com.wix.training.MainWindow._
 
-class MainWindow() extends JFrame(APP_TITLE){
-  private val sniperStatus:JLabel = createLabel(Main.STATUS_JOINING)
-
+class MainWindow() extends JFrame(APP_TITLE) {
+  private val sniperStatus: JLabel = createLabel(Main.STATUS_JOINING)
 
   setName(MAIN_WINDOW_NAME)
   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
   add(sniperStatus)
-
-
+  pack()
   setVisible(true)
 
 
   def createLabel(initialText: String): JLabel = {
     val result = new JLabel(initialText)
-    result.setName(MainWindow.SNIPER_STATUS_NAME)
+    result.setName(SNIPER_STATUS_NAME)
     result.setBorder(new LineBorder(Color.BLACK))
     result
   }
 
+  def showStatus(status: String): Unit = {
+    sniperStatus.setText(status)
+  }
 }
 
 object MainWindow {
   val MAIN_WINDOW_NAME = "Auction Sniper Main"
   val APP_TITLE = "Auction Sniper"
   val SNIPER_STATUS_NAME = "sniper-status"
-  //  val SNIPERS_TABLE_NAME = "Snipers Table"
-  //  val NEW_ITEM_ID_NAME = "new-item-id"
-  //  val NEW_ITEM_STOP_PRICE_NAME = "new-item-stop-price"
-  //  val JOIN_BUTTON_NAME = "join-button"
 }
+
+
